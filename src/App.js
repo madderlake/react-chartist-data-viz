b982740beb52a6c5f239d4526e437ed2093fd4d9
madderlake <mary@maryahayne.com>
2020-07-15 17:44:36 -0700

    add v1, update css to v2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.scss';
-import IncomeInequalityChart2 from './components/IncomeInequality2/IncInequality';
+import IncomeInequalityChart2 from './components/IncomeInequality/IncInequality';
 
 function App() {
   return (
