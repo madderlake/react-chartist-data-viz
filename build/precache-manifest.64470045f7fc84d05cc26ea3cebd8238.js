6ef8c2db0eb795c43a81986877eb67f8022d6a22
madderlake <mary@maryahayne.com>
2020-07-15 20:12:53 -0700

    add build directory for realz

---
+++ build/precache-manifest.64470045f7fc84d05cc26ea3cebd8238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b782001c7185b8aa442464cc8d7ae195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7ee4cbc4b5dc6099b60",
+    "url": "/static/css/2.9b47810d.chunk.css"
+  },
+  {
+    "revision": "f11291658023c154ded3",
+    "url": "/static/css/main.146d1378.chunk.css"
+  },
+  {
+    "revision": "b7ee4cbc4b5dc6099b60",
+    "url": "/static/js/2.7995bd5e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7995bd5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11291658023c154ded3",
+    "url": "/static/js/main.02863f89.chunk.js"
+  },
+  {
+    "revision": "3bf5278b7cf48dc09124",
+    "url": "/static/js/runtime-main.f9904745.js"
+  }
+]);
