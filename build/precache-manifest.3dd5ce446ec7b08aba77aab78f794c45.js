7f7d6225de41b190101ff93275a8c4f13e4bf0c3
madderlake <mary@maryahayne.com>
2020-08-08 18:33:32 -0700

    rm yarn.lock

---
+++ build/precache-manifest.3dd5ce446ec7b08aba77aab78f794c45.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972732dff34a03b837aa1d5cddb92e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f58e45e25ccbaea2269",
+    "url": "/static/css/2.972262ac.chunk.css"
+  },
+  {
+    "revision": "090642e83ce494496266",
+    "url": "/static/css/main.2865d207.chunk.css"
+  },
+  {
+    "revision": "8f58e45e25ccbaea2269",
+    "url": "/static/js/2.bd3deb38.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.bd3deb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "090642e83ce494496266",
+    "url": "/static/js/main.64c16228.chunk.js"
+  },
+  {
+    "revision": "197956e7078bb9b60c9f",
+    "url": "/static/js/runtime-main.429d8d23.js"
+  },
+  {
+    "revision": "b10bf95637fd483df48330e4e04baacc",
+    "url": "/static/media/3-transportation-on.b10bf956.svg"
+  },
+  {
+    "revision": "6eb7b844523cfb826a3477827fd195c3",
+    "url": "/static/media/3-transportation.6eb7b844.svg"
+  },
+  {
+    "revision": "c3524c771639e0a841e345baa7007ae1",
+    "url": "/static/media/4-community-dev-on.c3524c77.svg"
+  },
+  {
+    "revision": "d349ad6f4e0fee9bff1f793522827237",
+    "url": "/static/media/4-community-dev.d349ad6f.svg"
+  },
+  {
+    "revision": "60a1e1601e7859abf09ab26050b1adcf",
+    "url": "/static/media/6-health-on.60a1e160.svg"
+  },
+  {
+    "revision": "743bb1d190dc3b4c4e96d76047872292",
+    "url": "/static/media/6-health.743bb1d1.svg"
+  },
+  {
+    "revision": "25498e84a15c0e91e12e9f4868ec54be",
+    "url": "/static/media/7-income-security-on.25498e84.svg"
+  },
+  {
+    "revision": "39a7205f28cfb8a11e891769e58ebed3",
+    "url": "/static/media/7-income-security.39a7205f.svg"
+  }
+]);
