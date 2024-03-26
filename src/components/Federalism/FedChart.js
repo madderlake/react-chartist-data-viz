93d98483f91c1884b74fbbea3e17792062e28be2
madderlake <mary@maryahayne.com>
2024-03-26 16:09:07 -0700

    remove FedChart

--- src/components/Federalism/FedChart.js
+++
@@ -1,8 +0,0 @@
-import { BarChart } from 'chartist';
-import { useCallback } from 'react';
-export const FedChart = (data, options, responsiveOptions, handler) => {
-  const chart = new BarChart('#chart', data, options, responsiveOptions);
-  chart.on('draw', handler);
-  // console.log(chart());
-  return chart;
-};
